349fe30c1c2c2611c7192fc34b604d6686bec99e
Anthony Murphy <anthony@asmurph.onmicrosoft.com>
2021-03-17 12:23:56 -0500

    First Commit

---
+++ src/webparts/connectToMicrosoftGraphApi/ConnectToMicrosoftGraphApiWebPart.ts
@@ -0,0 +1,53 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './ConnectToMicrosoftGraphApiWebPart.module.scss';
+import * as strings from 'ConnectToMicrosoftGraphApiWebPartStrings';
+import { MSGraphClient } from '@microsoft/sp-http';
+
+export interface IConnectToMicrosoftGraphApiWebPartProps {
+  description: string;
+}
+
+export default class ConnectToMicrosoftGraphApiWebPart extends BaseClientSideWebPart<IConnectToMicrosoftGraphApiWebPartProps> {
+
+  public render(): void {
+
+    this.context.msGraphClientFactory
+    .getClient()
+    .then((client: MSGraphClient): void => {
+      // get information about the current user from the Microsoft Graph
+      client
+        .api('/me')
+        .get((error, response: any, rawResponse?: any) => {
+          // handle the response
+          console.log(JSON.stringify(response));
+
+    this.domElement.innerHTML = `
+      <div class="${ styles.connectToMicrosoftGraphApi }">
+        <div class="${ styles.container }">
+          <div class="${ styles.row }">
+            <div class="${ styles.column }">
+              <span class="${ styles.title }">Welcome to SharePoint!</span>
+              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
+              <p class="${ styles.description }">${escape(this.properties.description)}</p>
+              <a href="https://aka.ms/spfx" class="${ styles.button }">
+                <span class="${ styles.label }">Learn more</span>
+              </a>
+            </div>
+            <p class="${ styles.description }">DisplayName:${response.displayName}</p>
+      <p class="${ styles.description }">Email:${response.mail}</p>
+      <p class="${ styles.description }">Phone Number:${response.businessPhones[0]}</p>
+      </div>
+          </div>
+        </div>
+      </div>`;
+        });
+      });
+} 
+}
